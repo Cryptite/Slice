c815f4d6f611e4f68c86d7e1c8e3ce71c2bbc3cf
cryptite <cryptite@gmail.com>
2021-11-23 02:12:04 -0600

    Update paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.14"
+    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.2.0"
 }
 
 repositories {
