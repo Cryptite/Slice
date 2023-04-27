3544d87a4584a80cdec65f605c258cc82b8e2f0d
Cryptite <cryptite@gmail.com>
2023-04-27 11:36:25 -0500

    Various dep/upstream updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.4"
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.5"
 }
 
 val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
